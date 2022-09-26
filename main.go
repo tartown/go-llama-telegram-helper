282a0c439374fe741917146a80bfe7fad0179b12
tartown <126340186+tartown@users.noreply.github.com>
2022-09-26 12:12:44 +0000

    Version 2.3.3

--- main.go
+++ main.go
@@ -73,4 +73,5 @@ func main() {
 	}
 
 	// Init LLAMA binding
-	l, err = llama.New(modelPath, llama.SetContext(1024), llama.EnableEmbeddings, llama.Enable
+	l, err = llama.New(modelPath, llama.SetContext(1024), llama.EnableEmbeddings, llama.EnableMLock)
+	if err != nil {

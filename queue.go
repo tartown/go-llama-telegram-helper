6997091fc49c08f2e5730703487f8546543d189d
tartown <126340186+tartown@users.noreply.github.com>
2022-10-13 22:33:38 +0000

    New translations en.json (French)

--- queue.go
+++ queue.go
@@ -16,3 +16,28 @@ func NewTaskQueue(limit int) *TaskQueue {
 		tasks: make([]*Task, 0),
 		users: make(map[int64]*Task, 0),
 		Limit: limit,
+	}
+}
+
+
+// Get task by UserID and its count in queue
+func (q *TaskQueue) Load(userId int64) (*Task, int) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	for n, task := range q.tasks {
+		if task.UserID == userId {
+			return task, n
+		}
+	}
+
+	return nil, -1
+}
+
+
+func (q *TaskQueue) Enqueue(task *Task) (int, error) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	t, exists := q.users[task.UserID]
+	if exists {
